9fa9e5333d79ac7f3389814b0e4b2d1b67a80d8c
Jason Bahl <jasonbahl@mac.com>
2018-09-26 12:21:00 -0600

    - Step 1 starting point
    The bulk of the App is in App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,93 @@
 import React, {Component} from 'react';
-import AppLayout from './components/AppLayout'
-import ArticleGrid from './components/ArticleGrid'
-import { ApolloProvider } from 'react-apollo';
-import { client } from './state';
+import {Row, Col, Card, Divider, Spin, Avatar, Button} from 'antd';
+import AppLayout from './components/AppLayout';
+import {ApolloProvider} from 'react-apollo';
+import {client} from './state';
+import {Query} from 'react-apollo';
+import gql from 'graphql-tag';
+import Moment from 'react-moment';
+
+const {Meta} = Card;
+
+const QUERY_GET_POSTS = gql`
+query GET_POSTS($first: Int) {
+  posts(first: $first) {
+    pageInfo {
+      hasNextPage
+      endCursor
+    }
+    edges {
+      node {
+        id
+        title
+        link
+        featuredImage {
+          caption
+          sourceUrl
+        }
+        date
+        authors {
+          nodes {
+            firstName
+            lastName
+          }
+        }
+      }
+    }
+  }
+}
+`;
+
+export const QUERY_FIRST_AMOUNT = 100;
 
 class App extends Component {
     render() {
         return (
             <ApolloProvider client={client}>
                 <AppLayout>
-                    <ArticleGrid {...this.props} />
+                    <Query
+                        query={QUERY_GET_POSTS}
+                        fetchPolicy="cache-and-network"
+                        variables={{
+                            first: QUERY_FIRST_AMOUNT
+                        }}
+                    >
+                        {({loading, error, fetchMore, data: {posts}}) => {
+                            if (loading && (!posts || !posts.edges)) return <div
+                                style={{textAlign: 'center'}}><Spin size="large"/></div>;
+                            if (error) return `Error! ${error.message}`;
+
+                            return (
+                                <Row type="flex" justify="space-around">
+                                    {posts.edges.map(edge => (
+                                        <Col key={edge.node.id} span={6}>
+                                            <a target="_blank" href={edge.node.link}>
+                                                <Card
+                                                    hoverable
+                                                    style={{margin: '10px'}}
+                                                    cover={edge.node.featuredImage && <img
+                                                        alt={edge.node.featuredImage.caption ? edge.node.featuredImage.caption : null}
+                                                        style={{maxWidth: '100%'}}
+                                                        src={edge.node.featuredImage.sourceUrl ? edge.node.featuredImage.sourceUrl : null}/>}
+                                                >
+                                                    <Meta
+                                                        avatar={<Avatar
+                                                            src='https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png'/>}
+                                                        title={edge.node.authors.nodes[0].firstName + ' ' + edge.node.authors.nodes[0].lastName}
+                                                        description={<Moment
+                                                            format="MMM DD, YYYY">{edge.node.date}</Moment>}
+                                                    />
+                                                    <Divider dashed/>
+
+                                                    <h3 dangerouslySetInnerHTML={{__html: edge.node.title}}/>
+                                                </Card>
+                                            </a>
+                                        </Col>
+                                    ))}
+                                </Row>
+                            );
+                        }}
+                    </Query>
                 </AppLayout>
             </ApolloProvider>
         );
--- src/state/index.js
+++ src/state/index.js
@@ -1,5 +1,5 @@
 import ApolloClient from "apollo-boost";
 
 export const client = new ApolloClient({
-    uri: "http://localhost:9002/graphql"
+    uri: "https://www.denverpost.com/graphql"
 });
